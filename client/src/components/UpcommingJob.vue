3ccf5df947846d10531c527e5846ea30730001d8
Romian Tairovski <romian.tairovski@gmail.com>
2020-10-16 01:32:11 +0200

    upcomming job view. Still not the way I want it, because it now just shows the job with the highest date. Need to show the closest date in the future of the current date.

--- client/src/components/UpcommingJob.vue
+++ client/src/components/UpcommingJob.vue
@@ -1,18 +1,34 @@
 <template>
-    <div class="border">
-        Up and comming
-        <job item="2"></job>
+    <div class="">
+        <h3>Next up</h3>
+        <job :item="this.job['job_id']" variant="medium"></job>
     </div>
 </template>
 
 <script>
     import Job from "./items/Job";
+    import axios from "axios";
     export default {
         name: "UpcommingJob",
         components: {
             Job
+        },
+        data () {
+            return {
+                job: null
+            }
+        },
+        async mounted() {
+            await axios
+                .post('http://192.168.1.200/YoungOnes/API/Jobs/Get', {
+                    "order": "job_date",
+                    "sort": "DESC"
+                }).then((response) => {
+                    this.job = response.data.return[0]
+                }).catch(error => console.log(error))
         }
-    }
+     }
+
 </script>
 
 <style scoped>

7ab5bfdda35c15be518cc6bc65a5270d7c7dd09c
Romian Tairovski <romian.tairovski@gmail.com>
2021-03-11 11:41:53 +0100

    [💥] Add login

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// @ts-ignore
+import gAuthPlugin from 'vue3-google-oauth2';
+let gauthClientId = "263402996731-85sjjl8mdbohi028lorhh55kviqvalmp.apps.googleusercontent.com";
 
 createApp(App)
     .use(store)
     .use(router)
+    .use(gAuthPlugin, { clientId: gauthClientId, scope: 'email', prompt: 'consent', fetch_basic_profile: true })
     .mount('#app')

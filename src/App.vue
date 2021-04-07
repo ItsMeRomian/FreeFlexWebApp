0bd3ecfa8ce562809e9a6704775a3c67b6b458d6
Romian Tairovski <romian.tairovski@gmail.com>
2021-04-07 15:14:02 +0200

    [✨] Add Footer

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,19 @@
   <div class="container">
     <router-view/>
   </div>
+  <Footer/>
 </template>
 
 <style>
+  a {
+    color: #99c2ff;
+  }
 </style>
 <script>
   import Menu from "@/components/Menu";
+  import Footer from "@/components/Footer"
   export default {
-    components: {Menu},
+    components: {Menu, Footer},
     mounted() {
       //if (!this.$store.state.loggedIn) this.$router.push('/login')
       console.log(this.$store.state)
---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <hr>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col">
+                <h3>FreeFlexr</h3>
+                <p>Made By <a href="https://dynafools.live/" target="_blank">ItsMeRomian</a>, with &hearts;</p>
+            </div>
+            <div class="col text-center">
+                <h3>Github</h3>
+                <p>Follow the development process on the <a href="https://github.com/itsmeromian/FreeFlexWebApp" target="_blank">FreeFlexr repo</a>.</p>
+            </div>
+            <div class="col text-end">
+                <h3>Found a bug? Want a feature?</h3>
+                <p>You can make an issue on Github <a href="https://github.com/itsmeromian/FreeFlexWebApp/issues" target="_blank">here</a>.</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+
+</style>

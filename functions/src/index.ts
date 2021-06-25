b70e4656a1cbe2e8c921f7a61afc85b82e4a2703
Romian Tairovski <romian.tairovski@gmail.com>
2021-06-25 10:07:52 +0200

    Disable functions for now

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
-import * as functions from "firebase-functions";
+// import * as functions from "firebase-functions";
 
-export const api = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+// export const api = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", { structuredData: true });
+//   response.send("Hello from Firebase!");
+// });
